daaf62bfa60380a969234257a0f28f388fefe508
Klement kriszta <krisztafa@gmail.com>
2017-10-06 23:15:52 +0200

    js repair

--- script.js
+++ script.js
@@ -8,16 +8,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "url('https://st.depositphotos.com/3470617/4869/v/950/depositphotos_48698491-stock-illustration-big-food-mix-pattern.jpg') repeat center;";
-}
